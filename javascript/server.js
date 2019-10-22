1b1e102c788e727efcaf459e51ca0a1fab75cca7
GabrielCMCR <gabrielred.cm@gmail.com>
2019-10-22 17:21:06 -0600

    hole hole

--- javascript/server.js
+++ javascript/server.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const config = require('config');
 const app = express();
 
-//mongoose.Promise = global.Promise;
 
 const db = config.get('mongoURI');
 mongoose
   .connect(db, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
+
+
